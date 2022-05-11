9be1757de94b16db41d67dc60cacde56affb8f9c
Meet110201 <meet.parmeet11@gmail.com>
2022-05-12 00:42:53 +0530

    roles in our team is changed

--- index.php
+++ index.php
@@ -32,33 +32,35 @@ if (isset($_POST['logout'])) {
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Food Donation Point</title>
-   <link rel="stylesheet" href="style/style.css?version=">
-   <link rel="stylesheet" href="./style/team-style.css?version=">
+   <link rel="stylesheet" href="style/style.css?version=1">
+   <link rel="stylesheet" href="./style/team-style.css?version=1">
    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
-   <header>
-      <div class="logo">
-         <a href="index.html"></a>
-         <img src="./img/logo-white.png" alt="">
-      </div>
-      <ul>
-         <?php if (empty($_SESSION['email']) && empty($_SESSION['admin_email'])) { ?>
-            <li><a href="userlogin.php">LOGIN</a></li>
-            <li><a href="signup.php">SIGN UP</a></li>
-            <li><a href="contact.html">CONTACT US</a></li>
-         <?php } else { ?>
-            <li><a href="logout.php">LOGOUT</a></li>
-            <li><a href="contact.html">CONTACT US</a></li>
-            <?php if (empty($_SESSION['admin_email'])) { ?>
-               <li><a href="account.php">ACCOUNT -- <?php echo $_SESSION['email'] ?></a></li>
+   <div>
+      <header>
+         <div class="logo">
+            <a href="index.html"></a>
+            <img src="./img/logo-white.png" alt="">
+         </div>
+         <ul>
+            <?php if (empty($_SESSION['email']) && empty($_SESSION['admin_email'])) { ?>
+               <li><a href="userlogin.php">LOGIN</a></li>
+               <li><a href="signup.php">SIGN UP</a></li>
+               <li><a href="contact.html">CONTACT US</a></li>
             <?php } else { ?>
-               <li><a href="adminaccount.php">ADMIN ACCOUNT -- <?php echo $_SESSION['admin_email'] ?></a></li>
+               <li><a href="logout.php">LOGOUT</a></li>
+               <li><a href="contact.html">CONTACT US</a></li>
+               <?php if (empty($_SESSION['admin_email'])) { ?>
+                  <li><a href="account.php">ACCOUNT -- <?php echo $_SESSION['email'] ?></a></li>
+               <?php } else { ?>
+                  <li><a href="adminaccount.php">ADMIN ACCOUNT -- <?php echo $_SESSION['admin_email'] ?></a></li>
+               <?php } ?>
             <?php } ?>
-         <?php } ?>
-      </ul>
-   </header>
+         </ul>
+      </header>
+   </div>
    <br>
    <div>
       </ul>
@@ -90,48 +92,44 @@ if (isset($_POST['logout'])) {
                <img src="./img/parmeet-sq.jpeg" alt="our_team">
                <div class="social_media">
                   <div class="linkedin item"><a href="https://www.linkedin.com/in/singhparmeet011102/"><i class="fa fa-linkedin"></i></a></div>
-                  <div class="twitter item"><a href=""><i class="fa fa-twitter"></i></a></div>
                   <div class="instagram item"><a href="https://www.instagram.com/parmeet_singh.meet/"><i class="fa fa-instagram"></i></a></div>
                </div>
             </div>
-            <h3>Parmeet Singh</h3>
-            <h4>Team Leader</h4>
+            <h3>Parmeet <br> Singh</h3>
+            <h4>Front-end Developer <br> + <br> Front-end Tester</h4>
          </div>
          <div class="team_member">
             <div class="member_img">
                <img src="./img/chirag-sq.jpeg" alt="our_team">
                <div class="social_media">
                   <div class="linkedin item"><a href="https://www.linkedin.com/in/chirag-lala-830463190/"><i class="fa fa-linkedin"></i></a></div>
-                  <div class="twitter item"><a href=""><i class="fa fa-twitter"></i></a></div>
                   <div class="instagram item"><a href="https://www.instagram.com/_0chirag0_/"><i class="fa fa-instagram"></i></a></div>
                </div>
             </div>
-            <h3>Chirag Lala</h3>
-            <h4>Web Developer</h4>
+            <h3>Chirag <br> Lala</h3>
+            <h4>Back-end Developer <br> + <br>Back-end Tester</h4>
          </div>
          <div class="team_member">
             <div class="member_img">
                <img src="./img/khwahish-sq.jpg" alt="our_team">
                <div class="social_media">
                   <div class="linkedin item"><a href="https://www.linkedin.com/in/khwahish-agarwal-25a302214/"><i class="fa fa-linkedin"></i></a></div>
-                  <div class="twitter item"><a href=""><i class="fa fa-twitter"></i></a></div>
                   <div class="instagram item"><a href="https://www.instagram.com/khwahishhh____/"><i class="fa fa-instagram"></i></a></div>
                </div>
             </div>
-            <h3>Khwahish Agarwal</h3>
-            <h4>UI Designer</h4>
+            <h3>Khwahish <br> Agarwal</h3>
+            <h4>Front-end Developer <br> + <br> UI Designer</h4>
          </div>
          <div class="team_member">
             <div class="member_img">
                <img src="./img/yash-sq.jpeg" alt="our_team">
                <div class="social_media">
                   <div class="linkedin item"><a href="https://www.linkedin.com/in/yash-srivastava-506983201/"><i class="fa fa-linkedin"></i></a></div>
-                  <div class="twitter item"><a href=""><i class="fa fa-twitter"></i></a></div>
                   <div class="instagram item"><a href="https://www.instagram.com/_dr_dead/"><i class="fa fa-instagram"></i></a></div>
                </div>
             </div>
             <h3>Yash Srivastava</h3>
-            <h4>Web Developer</h4>
+            <h4>Data Management <br> + <br> Documentation</h4>
          </div>
       </div>
    </div>
